9e0c7706768d2ef2d4e671218214d7b7fb01f66d
Jacob Perron <jacob@openrobotics.org>
2020-07-07 20:56:06 -0700

    Imported upstream version '1.0.1' of 'upstream'

--- rmw_connext_cpp/src/connext_static_subscriber_info.cpp
+++ rmw_connext_cpp/src/connext_static_subscriber_info.cpp
@@ -17,11 +17,11 @@
 #include "rmw_connext_shared_cpp/qos.hpp"
 
 rmw_ret_t ConnextStaticSubscriberInfo::get_status(
-  rmw_event_type_t event_type,
+  DDS::StatusMask mask,
   void * event)
 {
-  switch (event_type) {
-    case RMW_EVENT_LIVELINESS_CHANGED:
+  switch (mask) {
+    case DDS::StatusKind::DDS_LIVELINESS_CHANGED_STATUS:
       {
         DDS::LivelinessChangedStatus liveliness_changed;
         DDS::ReturnCode_t dds_return_code =
@@ -42,7 +42,7 @@ rmw_ret_t ConnextStaticSubscriberInfo::get_status(
 
         break;
       }
-    case RMW_EVENT_REQUESTED_DEADLINE_MISSED:
+    case DDS::StatusKind::DDS_REQUESTED_DEADLINE_MISSED_STATUS:
       {
         DDS::RequestedDeadlineMissedStatus requested_deadline_missed;
         DDS::ReturnCode_t dds_return_code =
@@ -61,7 +61,7 @@ rmw_ret_t ConnextStaticSubscriberInfo::get_status(
 
         break;
       }
-    case RMW_EVENT_REQUESTED_QOS_INCOMPATIBLE:
+    case DDS::StatusKind::DDS_REQUESTED_INCOMPATIBLE_QOS_STATUS:
       {
         DDS::RequestedIncompatibleQosStatus requested_incompatible_qos;
         DDS::ReturnCode_t dds_return_code =
@@ -80,23 +80,6 @@ rmw_ret_t ConnextStaticSubscriberInfo::get_status(
         rmw_requested_qos_incompatible->last_policy_kind = dds_qos_policy_to_rmw_qos_policy(
           requested_incompatible_qos.last_policy_id);
 
-        break;
-      }
-    case RMW_EVENT_MESSAGE_LOST:
-      {
-        DDS::SampleLostStatus sample_lost_status;
-
-        DDS::ReturnCode_t dds_return_code =
-          topic_reader_->get_sample_lost_status(sample_lost_status);
-        rmw_ret_t from_dds = check_dds_ret_code(dds_return_code);
-        if (RMW_RET_OK != from_dds) {
-          return from_dds;
-        }
-
-        rmw_message_lost_status_t * rmw_message_lost =
-          static_cast<rmw_message_lost_status_t *>(event);
-        rmw_message_lost->total_count = sample_lost_status.total_count;
-        rmw_message_lost->total_count_change = sample_lost_status.total_count_change;
         break;
       }
     default:
